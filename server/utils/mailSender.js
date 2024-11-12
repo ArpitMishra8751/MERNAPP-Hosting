02bb7fe4f2891a38ac2bba8ca1eefd2f01634fd2
Arpit Mishra <111645498+ArpitMishra8751@users.noreply.github.com>
2024-11-12 14:41:20 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -21,7 +21,8 @@ const mailSender = async(email,title,body)=>{
             subject:`${title}`,
             html:`${body}`,
         };
-        let info = await transporter.sendMail(mailOptions,function(error,info){
+        // let info = await 
+        transporter.sendMail(mailOptions,function(error,info){
             if(error){
                 console.log("some err occured... ",error);
             }
@@ -29,8 +30,8 @@ const mailSender = async(email,title,body)=>{
                 console.log("email send successfully");
             }
         })
-        console.log( "information.. " ,info);
-        return info;
+        // console.log( "information.. " ,info);
+        // return info;
     }
     catch(err){
         console.log("Some error occuring while sending email ... ",err);
